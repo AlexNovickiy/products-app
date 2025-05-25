ca35cdf5130c56e1e96280f2a47b76a28cc0a972
AlexNovickiy <novickiisasha78@gmail.com>
2025-05-26 02:51:58 +0300

    added filter with render products

--- src/home.js
+++ src/home.js
@@ -1,1 +1,24 @@
+import { refs } from './js/refs';
+import {
+  renderFunctionCategories,
+  renderFunctionProducts,
+} from './js/render-function';
+import { getProductsCategories, getProducts } from './js/products-api';
+import { onCategoryClick } from './js/handlers';
+
 //Логіка сторінки Home
+
+renderCategoriesList();
+renderProductsList();
+
+async function renderCategoriesList() {
+  const categoriesList = await getProductsCategories();
+  renderFunctionCategories(categoriesList);
+}
+
+async function renderProductsList() {
+  const productsList = await getProducts();
+  renderFunctionProducts(productsList);
+}
+
+refs.categoriesList.addEventListener('click', onCategoryClick);
--- src/js/render-function.js
+++ src/js/render-function.js
@@ -1,1 +1,55 @@
-//Функцію для створення, рендеру або видалення розмітки
+//Функції  для створення, рендеру або видалення розмітки
+import { refs } from './refs.js';
+
+export function renderFunctionCategories(categoriesList) {
+  refs.categoriesList.innerHTML = categoriesList
+    .map(category => {
+      return `<li class="categories__item">
+   <button class="categories__btn" type="button">${category}</button>
+ </li>`;
+    })
+    .join('');
+}
+
+export function renderFunctionProducts(productsList) {
+  refs.productsList.innerHTML = productsList
+    .map(product => {
+      return `<li class="products__item" data-id="${product.id}">
+    <img class="products__image" src="${product.images[0]}" alt="${product.title}"/>
+    <p class="products__title">${product.title}</p>
+    <p class="products__brand"><span class="products__brand--bold">Brand:</span> ${product.brand}</p>
+    <p class="products__category">Category: ${product.category}</p>
+    <p class="products__price">Price: $${product.price}</p>
+ </li>
+`;
+    })
+    .join('');
+}
+
+export function appendFunctionProducts(productsList) {
+  refs.productsList.insertAdjacentHTML(
+    'beforeend',
+    productsList
+      .map(product => {
+        return `<li class="products__item" data-id="${product.id}" data-category="${product.category}">
+    <img class="products__image" src="${product.images[0]}" alt="${product.title}"/>
+    <p class="products__title">${product.title}</p>
+    <p class="products__brand"><span class="products__brand--bold">Brand:</span> ${product.brand}</p>
+    <p class="products__category">Category: ${product.category}</p>
+    <p class="products__price">Price: $${product.price}</p>
+ </li>
+`;
+      })
+      .join('')
+  );
+}
+
+export function removesProductsByCategory(category) {
+  const products = Array.from(refs.productsList.children);
+  products.forEach(product => {
+    const productCategory = product.dataset.category.trim().toLowerCase();
+    if (productCategory === category) {
+      product.remove();
+    }
+  });
+}
